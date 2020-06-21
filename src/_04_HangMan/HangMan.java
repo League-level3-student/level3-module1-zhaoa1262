7c7c06c801e7c9429b3e9da53239c4ed16d4d660
zhaoamy <zhaoamy@amys-air-2.lan>
2020-06-20 17:24:11 -0700

    --

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,9 +1,46 @@
 package _04_HangMan;
 
-import javax.swing.JOptionPane;
+import java.util.Stack;
 
-public class HangMan{
-	String numOfWordsQuestion = JOptionPane.showInputDialog("How many words?");
-	int numOfWords = Integer.parseInt(numOfWordsQuestion);
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan {
+	static String numOfWordsQuestion = JOptionPane.showInputDialog("How many words?");
+	static int numOfWords = Integer.parseInt(numOfWordsQuestion);
+	static Stack<String> words = new Stack<String>();
+	//static int count = 0;
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+
+	public static void main(String[] args) {
+		for (int i = 0; i < numOfWords; i++) {
+			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+			words.pop();
+	}
+	}
+	void run() {
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.add(label);
+		
+		for(int i = 0; i<words.pop().length(); i++) {
+			label.add("_", label);
+		
+		}
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	
+	/*for( int i = 0; i < numOfWords; i++) {
+	for(int j = 0 ; j < numOfWords; j++) {
+		count++;
+		if(words.get(j).equals(words.get(i)))
+		{
+			
+		}	
+	}
+}*/
 }
---
+++ src/_04_HangMan/HangManRunner.java
@@ -0,0 +1,9 @@
+package _04_HangMan;
+
+import _03_IntroToStacks._02_TextUndoRedo;
+
+public class HangManRunner {
+	public static void main(String[] args) {
+	new HangMan().run();
+	}
+}
